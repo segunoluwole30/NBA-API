4f034fe1a2db70c618678d95a647c4c358eaa856
Segun Oluwole <segunoluwole30@gmail.com>
2025-05-23 16:32:33 -0500

    added query for looking up player in database by name

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from database import SessionLocal
 from models import Player
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 
 app = FastAPI()
 
@@ -15,3 +16,11 @@ def get_db():
 @app.get("/players")
 def read_players(db: Session = Depends(get_db)):
     return db.query(Player).all()
+
+@app.get("/players/{player_name}")
+def read_player(player_name: str, db: Session = Depends(get_db)):
+    player = db.query(Player).filter(func.lower(Player.name) == player_name.lower()).first()
+    if player:
+        return player
+    else:
+        return {"error": "Player not found"}
